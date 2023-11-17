ffcfe604a4e090b8b799416d5877938dbef538eb
Rahadian Yusuf <rahadian.y@gmail.com>
2023-11-17 10:22:00 +0700

    fix: move routers

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,9 +4,7 @@ import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 
-import homeRouters from './routers/home.js'
-import assetsRouters from './routers/assets.js'
-import chatRouters from './routers/chatgpt.js'
+import { attachRouters } from './routers/index.js'
 
 if (process.env.NODE_ENV === 'production') {
   dotenv.config({
@@ -20,17 +18,8 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = Number(process.env.PORT ?? 3000)
 const hostname = process.env.HOSTNAME ?? 'localhost'
-const publicPath = path.join(path.resolve(), '../client/public')
-const distPath = path.join(path.resolve(), 'dist')
 
 const app = express()
-
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(distPath))
-} else {
-  app.use('/', express.static(publicPath))
-  app.use('/src', assetsRouters)
-}
 
 const whitelist = ['http://localhost:3000', 'http://localhost:5173']
 
@@ -56,9 +45,8 @@ app.options(
   })
 )
 
-app.use('/api/chat', chatRouters())
 
-app.use(homeRouters)
+attachRouters(app)
 
 app.listen(port, hostname, () => {
   console.log('Server listening on port', port)
--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -1,5 +1,23 @@
+import path from 'path'
+
+import express from 'express'
+
 import homeRouters from './home.js'
 import assetsRouters from './assets.js'
 import chatRouters from './chatgpt.js'
 
-import express from 'express'
+const publicPath = path.join(path.resolve(), '../client/public')
+const distPath = path.join(path.resolve(), 'dist')
+
+export function attachRouters(app: express.Express): void {
+  if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(distPath))
+  } else {
+    app.use('/', express.static(publicPath))
+    app.use('/src', assetsRouters)
+  }
+
+  app.use('/api/chat', chatRouters())
+
+  app.use(homeRouters)
+}
