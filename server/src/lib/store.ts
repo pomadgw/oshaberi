fd84fa2004205da4d2e4f11f3423f6225edfa9e0
Rahadian Yusuf <rahadian.y@gmail.com>
2023-12-31 09:51:55 +0700

    feat: add Redis caching for embeddings

--- server/src/lib/store.ts
+++ server/src/lib/store.ts
@@ -1,24 +1,61 @@
 import { HNSWLib } from 'langchain/vectorstores/hnswlib'
 import { type Document } from 'langchain/document'
+import { Redis } from 'ioredis'
+import { CacheBackedEmbeddings } from 'langchain/embeddings/cache_backed'
+import { RedisByteStore } from 'langchain/storage/ioredis'
 
 import { type OshaberiValidLLMProvider } from '../types'
+
+const redisClient = new Redis(process.env.REDIS_URL ?? 'redis://localhost:6379')
+const redisStore = new RedisByteStore({
+  client: redisClient
+})
+
+const cacheBackedEmbeddings: Record<OshaberiValidLLMProvider, CacheBackedEmbeddings | null> = {
+  openai: null,
+  ollama: null
+}
 
 const getEmbeddingClass = async (provider: OshaberiValidLLMProvider) => {
   if (provider === 'openai') {
     const { OpenAIEmbeddings } = await import('langchain/embeddings/openai')
-
-    return new OpenAIEmbeddings()
+    if (cacheBackedEmbeddings[provider] == null) {
+      cacheBackedEmbeddings[provider] = CacheBackedEmbeddings.fromBytesStore(
+        new OpenAIEmbeddings(),
+        redisStore,
+        {
+          namespace: 'openai'
+        }
+      )
+    }
   }
 
   if (provider === 'ollama') {
     const { OllamaEmbeddings } = await import('langchain/embeddings/ollama')
 
-    return new OllamaEmbeddings({ model: 'mistral-lower-gpu:latest' })
+    if (cacheBackedEmbeddings[provider] == null) {
+      cacheBackedEmbeddings[provider] = CacheBackedEmbeddings.fromBytesStore(
+        new OllamaEmbeddings(),
+        redisStore,
+        {
+          namespace: 'ollama'
+        }
+      )
+    }
   }
 
-  throw new Error('Invalid provider')
+  if (cacheBackedEmbeddings[provider] == null) {
+    throw new Error('Invalid provider')
+  }
+
+  return cacheBackedEmbeddings[provider]
 }
 
 export async function createVectorStore(documents: Document[], provider: OshaberiValidLLMProvider) {
-  return await HNSWLib.fromDocuments(documents, await getEmbeddingClass(provider))
+  const embeddings = await getEmbeddingClass(provider)
+  if (embeddings == null) {
+    throw new Error('Invalid provider')
+  }
+
+  return await HNSWLib.fromDocuments(documents, embeddings)
 }
